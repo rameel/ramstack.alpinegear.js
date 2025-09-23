86c85f4186784cba1b71e2d6b75c13a33d32751b
rameel <rameel-b@hotmail.com>
2025-09-23 05:26:07 +0500

    Defer x-format processing on nested x-data elements to avoid Alpine context errors

--- tests/playwright/x-format.spec.js
+++ tests/playwright/x-format.spec.js
@@ -41,3 +41,48 @@ test("x-format: context aware", async ({ page }) => {
     await expect(page.locator("#id_4")).toContainText("id_4");
     await expect(page.locator("#id_3")).toHaveAttribute("title", "id_3");
 });
+
+test("x-format: nested x-data", async ({ page }) => {
+    await set_html(page, `
+      <div x-data="{ value1: 3.14 }" x-format>
+        <div x-data="{ value2: 2.71 }">
+          <span id="v2">{{ value2 }}</span>
+          <button id="b2" @click="value2 = 'math.e'">Change</button>
+        </div>
+
+        <span id="v1">{{ value1 }}</span>
+        <button id="b1" @click="value1 = 'math.pi'">Change</button>
+      </div>`);
+
+    await expect(page.locator("#v1")).toContainText("3.14");
+    await expect(page.locator("#v2")).toContainText("2.71");
+
+    await page.locator("#b1").click();
+    await expect(page.locator("#v1")).toContainText("math.pi");
+
+    await page.locator("#b2").click();
+    await expect(page.locator("#v2")).toContainText("math.e");
+});
+
+test("x-format: nested x-data with manually x-format", async ({ page }) => {
+    await set_html(page, `
+      <div x-data="{ value1: 3.14 }" x-format>
+        <div x-data="{ value2: 2.71 }" x-format>
+          <span id="v2">{{ value2 }}</span>
+          <button id="b2" @click="value2 = 'math.e'">Change</button>
+        </div>
+
+        <span id="v1">{{ value1 }}</span>
+        <button id="b1" @click="value1 = 'math.pi'">Change</button>
+      </div>`);
+
+    await expect(page.locator("#v1")).toContainText("3.14");
+    await expect(page.locator("#v2")).toContainText("2.71");
+
+    await page.locator("#b1").click();
+    await expect(page.locator("#v1")).toContainText("math.pi");
+
+    await page.locator("#b2").click();
+    await expect(page.locator("#v2")).toContainText("math.e");
+});
+
